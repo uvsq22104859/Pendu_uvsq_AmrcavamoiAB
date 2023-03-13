8239e9c1be2aefa7d43c2d28cc234a7710fdf3c7
Guillaume <113714241+uvsq22205129@users.noreply.github.com>
2023-03-13 11:55:50 +0100

    ajouts d'un bouto

--- aff_mots.py
+++ aff_mots.py
@@ -18,28 +18,30 @@ def verify(lettre, mots):
     for i in range(len(mots[1])):
         if lettre == mots[1][i]:
             mots[0][i] = mots[1][i]
+        if mots[0] == mots[1]:
+            fenetre.destroy()
     affiche_mot(mots)
 
+def ver():
+    q = saisie.get()
+    verify(q, rep)
+    saisie.delete(0,len(q))
 
 fenetre =  tk.Tk()
 
 fenetre.geometry("900x600")
 
 label = tk.Label(fenetre)
+bouton1  = tk.Button(fenetre, command = ver)
 
 rec = tk.StringVar
 
-saisie = tk.Entry(fenetre, textvariable = rec)
+saisie = tk.Entry(fenetre, textvariable = rec, take = 'focus')
 
 rep = def_mot("bonjour")
 affiche_mot(rep)
 label.grid(row = 0, column = 4)
-print(rep)
-i = 0
-while(i < 7):
-    lettre = input("entrer une lettre :\t")
-    verify(lettre, rep)
-    i += 1
-    label.grid(row = 0, column = 4)
-fenetre.destroy()
+saisie.grid(row = 1, column = 3)
+bouton1.grid(row = 2, column = 5)
+
 fenetre.mainloop()
