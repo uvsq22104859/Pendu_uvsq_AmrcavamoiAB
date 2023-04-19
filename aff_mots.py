aed467e16d01dee2e3a2b6cc27b67caabcd9e284
Guillaume <113714241+uvsq22205129@users.noreply.github.com>
2023-04-19 17:06:48 +0200

    ajout d'un clavier

--- aff_mots.py
+++ aff_mots.py
@@ -42,11 +42,13 @@ def jeu(new):
         Et si le mot a ete trouver demande a l'utilisateur si il veut rejouer
         """
         global fenetre
+        in_word = False
         for i in range(len(mots[1])):
             if lettre == mots[1][i]:
                 mots[0][i] = mots[1][i]
+                in_word = True
             if mots[0] == mots[1]:
-                if tk.messagebox.askyesno(title="Fin de partie", message="Voulew vous refaire un partie?"):
+                if tk.messagebox.askyesno(title="Fin de partie", message="Voulez vous refaire un partie?"):
                     fenetre.destroy()
                     jeu(True)
                 else:
@@ -54,20 +56,43 @@ def jeu(new):
                 return
             saisie.delete(0,len(saisie.get()))
         affiche_mot(mots)
+        return in_word
 
     def ver():
         """
-        Verifie si l'utilisateur a bien mis une seule lettre
-        sinon affiche un message informatife rapelent qu'il faut mettre qu'une seule lettre 
+        Verifie si l'utilisateur a bien mis un nombre de lettres egal aux nombre de lettres du mots secret
+        sinon affiche un message informatife rapelent qu'il faut mettre le meme nombre de lettre 
         """
-        global score
-        nbr_lettre = len(saisie.get())
-        if nbr_lettre > 1 :
-            tk.messagebox.showinfo(title="Max elements 1", message="Vous ne pouvez entrer qu'une lettre a la fois.")
+        global score, fenetre, rep
+        mots = saisie.get()
+        nbr_lettre = len(mots)
+        print(rep[1])
+        if nbr_lettre != len(rep[0]) :
+            tk.messagebox.showinfo(title="Invalid entry", message="Votre reponse doit avoir le meme nombre de lettre que le mot a deviner.")
             saisie.delete(0, nbr_lettre)
             return
-        verify(saisie.get(), rep)
         score += 1
+        for i in range(nbr_lettre):
+            if mots[i] != rep[1][i]:
+                saisie.delete(0,len(mots))
+                return
+        if tk.messagebox.askyesno(title="Fin de partie", message="Voulez vous refaire un partie?"):
+            fenetre.destroy()
+            jeu(True)
+        else:
+            fenetre.destroy()
+        return
+    
+    def ver_lettre(event):
+        global score
+        if event.widget["bg"] == "green" or event.widget["bg"] == "red":
+            return
+        if verify(event.widget["text"], rep):
+            event.widget.config(bg="green")
+        else:
+            event.widget.config(bg="red")
+        score += 1
+
 
     def SaveParti():
         """
@@ -89,17 +114,6 @@ def jeu(new):
         verify(rep[1][lettre_indice[random.randint(0,len(lettre_indice)-1)]],rep)
 
 
-
-    def maxone():
-        """
-        empeche de mettre plus d'une lettre 
-        """
-        if len(saisie.get()) < 1:
-            return True
-        else:
-            saisie.delete(0, len(saisie.get()))
-            return True
-
     if(new):
         score = nbr_indice_util = 0
         fenetre =  tk.Tk()
@@ -113,21 +127,132 @@ def jeu(new):
     
      #Widgets
     label = tk.Label(fenetre, font=("Helvetica",30))
-    bouton1  = tk.Button(fenetre, text="OK", command = ver)
+    bouton_Ok  = tk.Button(fenetre, text="OK", command = ver)
     b_indice = tk.Button(fenetre, text = "indice", command = indice)
     b_Score = tk.Button(fenetre, text="Score", command=Tableau_score)
 
     rec = tk.StringVar
+    saisie = tk.Entry(fenetre, textvariable = rec, take = 'focus')
 
-    saisie = tk.Entry(fenetre, textvariable = rec, take = 'focus', validate = "key", validatecommand = maxone)
+      #Bouton lettres
+    bouton_a = tk.Button(fenetre,text="a")
+    bouton_a.bind('<Button-1>', ver_lettre)
 
-    #Positionnement
-    label.grid(row = 0, column = 4)
-    saisie.grid(row = 1, column = 3)
-    bouton1.grid(row = 2, column = 5)
-    b_indice.grid(row=3, column=6)
-    b_Score.grid(row=4, column=2)
+    bouton_b =tk.Button(fenetre, text="b")
+    bouton_b.bind('<Button-1>', ver_lettre)
 
+    bouton_c =tk.Button(fenetre, text="c")
+    bouton_c.bind('<Button-1>', ver_lettre)
+
+    bouton_d =tk.Button(fenetre, text="d")
+    bouton_d.bind('<Button-1>', ver_lettre)
+
+    bouton_e =tk.Button(fenetre, text="e")
+    bouton_e.bind('<Button-1>', ver_lettre)
+
+    bouton_f =tk.Button(fenetre, text="f")
+    bouton_f.bind('<Button-1>', ver_lettre)
+
+    bouton_g =tk.Button(fenetre, text="g")
+    bouton_g.bind('<Button-1>', ver_lettre)
+
+    bouton_h =tk.Button(fenetre, text="h")
+    bouton_h.bind('<Button-1>', ver_lettre)
+
+    bouton_i =tk.Button(fenetre, text="i")
+    bouton_i.bind('<Button-1>', ver_lettre)
+
+    bouton_j =tk.Button(fenetre, text="j")
+    bouton_j.bind('<Button-1>', ver_lettre)
+
+    bouton_k =tk.Button(fenetre, text="k")
+    bouton_k.bind('<Button-1>', ver_lettre)
+
+    bouton_l =tk.Button(fenetre, text="l")
+    bouton_l.bind('<Button-1>', ver_lettre)
+
+    bouton_m =tk.Button(fenetre, text="m")
+    bouton_m.bind('<Button-1>', ver_lettre)
+
+    bouton_n =tk.Button(fenetre, text="n")
+    bouton_n.bind('<Button-1>', ver_lettre)
+
+    bouton_o =tk.Button(fenetre, text="o")
+    bouton_o.bind('<Button-1>', ver_lettre)
+
+    bouton_p =tk.Button(fenetre, text="p")
+    bouton_p.bind('<Button-1>', ver_lettre)
+
+    bouton_q =tk.Button(fenetre, text="q")
+    bouton_q.bind('<Button-1>', ver_lettre)
+
+    bouton_r =tk.Button(fenetre, text="r")
+    bouton_r.bind('<Button-1>', ver_lettre)
+
+    bouton_s =tk.Button(fenetre, text="s")
+    bouton_s.bind('<Button-1>', ver_lettre)
+
+    bouton_t =tk.Button(fenetre, text="t")
+    bouton_t.bind('<Button-1>', ver_lettre)
+
+    bouton_u =tk.Button(fenetre, text="u")
+    bouton_u.bind('<Button-1>', ver_lettre)
+
+    bouton_v =tk.Button(fenetre, text="v")
+    bouton_v.bind('<Button-1>', ver_lettre)
+
+    bouton_w =tk.Button(fenetre, text="w")
+    bouton_w.bind('<Button-1>', ver_lettre)
+
+    bouton_x =tk.Button(fenetre, text="x")
+    bouton_x.bind('<Button-1>', ver_lettre)
+
+    bouton_y =tk.Button(fenetre, text="y")
+    bouton_y.bind('<Button-1>', ver_lettre)
+
+    bouton_z =tk.Button(fenetre, text="z")
+    bouton_z.bind('<Button-1>', ver_lettre)
+
+    #   Positionnement
+    label.grid(row = 0, column = 4, columnspan=5)
+    saisie.grid(row = 1, column = 0, columnspan=5)
+    bouton_Ok.grid(row = 1, column = 10)
+    b_indice.grid(row=10, column=7)
+    b_Score.grid(row=10, column=10)
+
+    #Positionnement lettres
+    #Ligne 1
+    bouton_a.grid(row = 3, column=1)    
+    bouton_b.grid(row = 3, column=2)    
+    bouton_c.grid(row = 3, column=3)    
+    bouton_d.grid(row = 3, column=4)    
+    bouton_e.grid(row = 3, column=5)    
+    bouton_f.grid(row = 3, column=6)
+    #Ligne 2
+    bouton_g.grid(row = 4, column=1)    
+    bouton_h.grid(row = 4, column=2)    
+    bouton_i.grid(row = 4, column=3)    
+    bouton_j.grid(row = 4, column=4)    
+    bouton_k.grid(row = 4, column=5)    
+    bouton_l.grid(row = 4, column=6)
+    #Ligne 3
+    bouton_m.grid(row = 5, column=1)    
+    bouton_n.grid(row = 5, column=2)    
+    bouton_o.grid(row = 5, column=3)    
+    bouton_p.grid(row = 5, column=4)    
+    bouton_q.grid(row = 5, column=5)    
+    bouton_r.grid(row = 5, column=6)
+    #Ligne 4  
+    bouton_s.grid(row = 6, column=1)    
+    bouton_t.grid(row = 6, column=2)
+    bouton_u.grid(row = 6, column=3)
+    bouton_v.grid(row = 6, column=4)
+    bouton_w.grid(row = 6, column=5)
+    bouton_x.grid(row = 6, column=6)
+    #ligne 5
+    bouton_y.grid(row = 7, column=3)
+    bouton_z.grid(row = 7, column=4)
+    
     #Petite blague
     def closed():
         """
@@ -177,10 +302,3 @@ def Tableau_score():
     fenetre.mainloop()
 
 start()
-# fenetre.destroy()
-
-# print("score: ",score, "nomnre d'indices utiliser: ", nbr_indice_util)
-
-# fenetre = tk.Tk()
-# 
-# fenetre.mainloop()
